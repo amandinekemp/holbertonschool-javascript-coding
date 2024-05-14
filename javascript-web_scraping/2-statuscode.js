65b7784669bc25ac54323523451de23a64bd9b39
DDoudine <amandinekemp@gmail.com>
2024-05-14 11:16:18 +0200

    Fix argument in console.error function

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -9,7 +9,7 @@ const url = process.argv[2];
 request(url, (error, response) => {
 	if(error) {
 		// If an error occurs, displays an error message
-		console.error('error:', error);
+		console.error(error);
 	} else {
 		// Otherwise, displays the HTTP status code
 		console.log('code:', response.statusCode);
