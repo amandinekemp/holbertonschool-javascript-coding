5b389b2709e54143ef713fb443d48403556759c8
DDoudine <amandinekemp@gmail.com>
2024-05-14 11:06:42 +0200

    Add script to display HTTP status code for a given URL

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+// Imports the request module for HTTP requests
+const request = require('request');
+// Retrieves the URL from command line arguments
+const url = process.argv[2];
+
+// Makes an HTTP request to the specified URL
+request(url, function (error, response, body) {
+	if (error) {
+		// If an error occurs, displays an error message
+		console.error('error:', error);
+	} else {
+		// Otherwise, displays the HTTP status code
+		console.log('code:', response.statusCode);
+	}
+});
