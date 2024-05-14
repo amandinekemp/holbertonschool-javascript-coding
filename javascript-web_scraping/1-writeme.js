d68ed0b35af1264a2b9512b250323e62862659e2
DDoudine <amandinekemp@gmail.com>
2024-05-14 10:44:26 +0200

    Add script to write data to a file

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+// Imports the fs module for file management
+const fs = require('fs');
+
+// Retrieves the file path from command line arguments
+const filePath = process.argv[2];
+// Retrieves the data to write to the file from command line arguments
+const data = process.argv[3];
+
+// Writes the data to the file with 'utf8' encoding
+fs.writeFile(filePath, data, 'utf8', (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+});
