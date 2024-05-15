f83dd6329a833775715b6a2bcbe406fdae9572f4
DDoudine <amandinekemp@gmail.com>
2024-05-15 09:41:24 +0200

    Count movies with Wedge Antilles from SWAPI

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+// Make a request to the URL
+request(url, function (error, response, body) {
+	// Handle error
+	if (error) {
+		console.error('error:', error);
+	} else {
+		// Parse JSON data from the response body
+		const films = JSON.parse(body).results;
+		// Initialize a counter for the number of movies with Wedge Antilles
+		let count = 0;
+		// Iterate over each film
+		films.forEach(film => {
+			// Iterate over each character in the film
+			film.characters.forEach(character => {
+				// Check if the character URL includes '/18/' (Wedge Antilles' ID)
+				if (character.includes('/18/')) {
+					count++;
+				}
+			});
+		});
+		// Log the number of movies with Wedge Antilles
+		console.log(count);
+	}
+});
