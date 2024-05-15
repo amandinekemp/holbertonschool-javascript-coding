54af6d7b610454ef1f563d548b018135c116dc04
DDoudine <amandinekemp@gmail.com>
2024-05-15 11:05:30 +0200

    Add comments

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -7,10 +7,13 @@ const url = process.argv[2];
 const filePath = process.argv[3];
 
 request(url, (error, response, body) => {
+	// Handle error
 	if (error) {
 		console.error(`Error: ${error}`);
 	} else {
+		// Write the response body to the file
 		fs.writeFile(filePath, body, 'utf-8', (error) => {
+			// Handle error
 			if (error) {
 				console.error(`Error: ${error}`);
 			}
