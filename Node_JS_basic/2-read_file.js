b896145ac271132f502395d318d4e88ec3e67d4e
DDoudine <amandinekemp@gmail.com>
2024-05-24 15:31:53 +0200

    Fix output

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -2,30 +2,24 @@ const fs = require('fs');
 
 function countStudents(path) {
   try {
-    // Read file and split into lines
-    const data = fs.readFileSync(path, 'utf8').trim().split('\n');
-    const students = {};
+    const data = fs.readFileSync(path, 'utf-8');
+    const lines = data.split('\n').filter(Boolean);
 
-    // Loop through each line
-    for (const line of data) {
-      const [field, firstname] = line.split(',');
-      // Add field and student if it doesn't exist
-      if (!students[field]) {
-        students[field] = { count: 0, list: [] };
-      }
-      students[field].count += 1;
-      students[field].list.push(firstname);
-    }
+    const students = lines.slice(1).map((line) => {
+      const [firstName, lastName, age, field] = line.split(',');
 
-    // Display the total number of students
-    console.log(`Number of students: ${Object.values(students).reduce((a, b) => a + b.count, 0)}`);
-    // Loop through each field
-    for (const field in students) {
-      // Display the number of students in the field and the list of first names
-      console.log(`Number of students in ${field}: ${students[field].count}. List: ${students[field].list.join(', ')}`);
-    }
-  } catch (error) {
-    // File read error
+      return {
+        firstName, lastName, age, field,
+      };
+    });
+
+    const csStudents = students.filter((student) => student.field === 'CS');
+    const sweStudents = students.filter((student) => student.field === 'SWE');
+
+    console.log(`Number of students: ${students.length}`);
+    console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.map((student) => student.firstName).join(', ')}`);
+    console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.map((student) => student.firstName).join(', ')}`);
+  } catch (err) {
     throw new Error('Cannot load the database');
   }
 }
