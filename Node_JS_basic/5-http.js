56c54520c2749f320f8a45e665a3f20a1c74f41e
DDoudine <amandinekemp@gmail.com>
2024-05-24 15:51:32 +0200

    Update server to use async/await and handle errors" (Mise à jour du serveur pour utiliser async/await et gérer les erreurs)

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,28 +1,37 @@
+// Import http module
 const http = require('http');
+// Import countStudents function
 const countStudents = require('./3-read_file_async');
 
-const database = process.argv[2];
-
-const app = http.createServer((req, res) => {
+// Create server
+const app = http.createServer(async (req, res) => {
+  // Set status code and content type
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
+
+  // Handle root route
   if (req.url === '/') {
     res.end('Hello Holberton School!');
+    // Handle students route
   } else if (req.url === '/students') {
-    countStudents(database)
-      .then((data) => {
-        res.write('This is the list of our students\n');
-        res.end(data);
-      })
-      .catch((error) => {
-        res.end(error.message);
-      });
+    res.write('This is the list of our students\n');
+    try {
+      // Get student count and list
+      const data = await countStudents(process.argv[2]);
+      // Send student list
+      res.end(`${data.join('\n').filter(Boolean)}`);
+    } catch (error) {
+      // Send error message
+      res.end(error.message);
+    }
+    // Handle 404
   } else {
-    res.statusCode = 404;
-    res.end();
+    res.end('Not Found\n');
   }
 });
 
+// Start server
 app.listen(1245);
 
+// Export server
 module.exports = app;
